a603438712df2b27051c3475134ac84c4e2f3b55
Filipe Vieira <filipe.silva@cidadaoon.com>
2023-01-27 17:58:50 -0300

    Criando a base e finalizando a api no strapi

---
+++ src/api/apiteste/routes/apiteste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apiteste router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::apiteste.apiteste');
---
+++ src/api/apiteste/services/apiteste.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apiteste service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::apiteste.apiteste');
